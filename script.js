ac77bd0944212a8c05449fde4a62479920adbcc2
Malachi I. Beerram <malachi.beerram@centre.edu>
2019-10-14 14:13:03 -0400

    10/14b

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 //main function for success 
 var codeMain = function(filmData)
 {
+    addtopageData(filmData);
+    
+    //var movieholder = 
+        
+        
     d3.select("#movie-holder")
+        .on("click",function(){
+                d3.select("#2baf70d1")
+                    .append("p")
+                    .selectAll("span")
+                    .attr("class","movie_description")
+                    .text(function(d){return d.description})
+                    })
+    
+    
+  
+}
+
+//code to add in and bind the data
+var addtopageData = function(filmData)
+{
+        d3.select("#movie-holder")
         .append("div")
         .selectAll("span")
         .data(filmData)
         .enter()
-        .append("span")
+        .append("div")
         .attr("class","movie_icon")
+        .attr("id",function(d){return d.id.split("-")[0]})
         .text(function(d){return d.title})
-    
-    
-    
 }
 
+// Promise to get data from API
 var dataPromise = d3.json("https://ghibliapi.herokuapp.com/films")
 dataPromise.then(
     function(filmData)
